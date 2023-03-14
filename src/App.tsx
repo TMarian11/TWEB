c52af7ddeab539e50360f26bb0930d2cec0aa796
TMarian11 <mariantabureanu@gmail.com>
2023-03-14 11:34:27 +0200

    Laborator 3

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => {
   const [inputValues, setInputValues] = useState<InputValues>({
     title: '',
     description: '',
-    imageUrl: '',
+    imageUrl: '', 
   });
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
