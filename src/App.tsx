160982b36dcd1511e2550a7c5eb4584004be5534
TMarian11 <mariantabureanu@gmail.com>
2023-05-03 15:25:59 +0300

    laboratorul 4, cred

--- src/App.tsx
+++ src/App.tsx
@@ -1,77 +1,66 @@
-import { useState } from 'react';
-import { Layout, Menu, Form, Input, Button, Card } from 'antd';
-import './App.css';
+import React from 'react';
+import './index.css';
 
-const { Header, Content } = Layout;
+interface User {
+    name: string;
+    email: string;
+    age: number;
+    address: string;
+    isActive: boolean;
+}
 
-type InputValues = {
-  title: string;
-  description: string;
-  imageUrl: string;
+interface AdvancedUser extends User {
+    education: string;
+    skills: string[];
+}
+
+const user: AdvancedUser = {
+    name: 'Taburaenu Marian',
+    email: 'mariantabureanu@gmail.com',
+    age: 21,
+    address: 'Orasul Chisinau',
+    isActive: true,
+    education: 'UTM',
+    skills: ['fotbal,tenis'],
 };
 
-const App = () => {
-  const [inputValues, setInputValues] = useState<InputValues>({
-    title: '',
-    description: '',
-    imageUrl: '', 
-  });
-
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInputValues({
-      ...inputValues,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  const handleSubmit = (values: InputValues) => {
-    console.log(values);
-    alert('Form submitted!');
-  };
-
-  const [form] = Form.useForm();
-
-  const cards = Array.from({ length: 1 }).map((_, index) => (
-    <Card key={index} style={{ margin: '16px 0' }}>
-      <h2>{inputValues.title}</h2>
-      <p>{inputValues.description}</p>
-      <img src={inputValues.imageUrl} alt={inputValues.title} style={{ maxWidth: '100%' }} />
-    </Card>
-  ));
-
-  return (
-    <Layout>
-      <Header>
-        <div className="logo" />
-        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-          <Menu.Item key="1">Pagina 1</Menu.Item>
-          <Menu.Item key="2">Pagina 2</Menu.Item>
-          <Menu.Item key="3">Pagina 3</Menu.Item>
-        </Menu>
-      </Header>
-      <Content style={{ margin: '0 16px' }}>
-        <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
-          <Form form={form} layout="vertical" onFinish={handleSubmit}>
-            <Form.Item label="Titlu" name="title">
-              <Input name="title" value={inputValues.title} onChange={handleInputChange} />
-            </Form.Item>
-            <Form.Item label="Descriere" name="description">
-              <Input name="description" value={inputValues.description} onChange={handleInputChange} />
-            </Form.Item>
-            <Form.Item label="Link poza" name="imageUrl">
-              <Input name="imageUrl" value={inputValues.imageUrl} onChange={handleInputChange} />
-            </Form.Item>
-            <Form.Item>
-              <Button type="primary" htmlType="submit">
-                Trimite
-              </Button>
-            </Form.Item>
-          </Form>
-          {cards}
+function Lab4() {
+    return (
+        <div className="container">
+          <div className="card">
+            <h1 className="title">Date Personale</h1>
+            
+                <p>
+                    <span className="label">Name:</span>
+                    <span className="value">{user.name}</span>
+                </p>
+                <p>
+                    <span className="label">Email:</span>
+                    <span className="value">{user.email}</span>
+                </p>
+                <p>
+                    <span className="label">Age:</span>
+                    <span className="value">{user.age}</span>
+                </p>
+                <p>
+                    <span className="label">Address:</span>
+                    <span className="value">{user.address}</span>
+                </p>
+                <p>
+                    <span className="label">Active:</span>
+                    <span className="value">{user.isActive.toString()}</span>
+                </p>
+                <p>
+                    <span className="label">Education:</span>
+                    <span className="value">{user.education}</span>
+                </p>
+                <p>
+                    <span className="label">Skills:</span>
+                    <span className="value">{user.skills.join(', ')}</span>
+                </p>
+            </div>
         </div>
-      </Content>
-    </Layout>
-  );
-};
+    );
+}
 
-export default App;
+export default Lab4;
