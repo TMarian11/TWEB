355db0ea8b2739b913a951b4bf0f3405665108e7
TMarian11 <mariantabureanu@gmial.com>
2023-05-05 01:36:44 +0300

    Lab 5.2

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,26 +1,172 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { useStore } from './Store';
+import './index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function MyComponent() {
+    const store = useStore();
+
+    const [localData, setLocalData] = useState<any>({ name: '', address: '', phone: '' });
+    const myData = store.myData;
+
+    const [isEditing, setIsEditing] = useState(false);
+    const [editingIndex, setEditingIndex] = useState(-1);
+
+    const handleAddObject = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        if (!localData.name || !localData.address || !localData.phone) {
+            alert('Introduceti toate campurile.');
+            return;
+        }
+
+        setIsLoading(true);
+        setTimeout(() => {
+            store.addObject(localData);
+            localStorage.setItem('myData', JSON.stringify(store.myData));
+            setIsLoading(false);
+        }, 2000);
+
+    };
+
+
+    const handleDeleteObject = (index: number) => {
+        store.deleteObject(index);
+        setLocalData({ name: '', address: '', phone: '' });
+        localStorage.setItem('myData', JSON.stringify(store.myData));
+    };
+
+    const handleUpdateObject = (index: number, newData: any) => {
+        store.updateObject(index, newData);
+        setIsLoading(true);
+        setTimeout(() => {
+            localStorage.setItem('myData', JSON.stringify(store.myData));
+            setIsLoading(false);
+        }, 2000);
+
+        // setează valorile din obiectul myData în starea localData
+        const dataToUpdate = store.myData[index];
+        setLocalData({ name: dataToUpdate.name, address: dataToUpdate.address, phone: dataToUpdate.phone });
+        setIsEditing(false);
+        setEditingIndex(-1);
+    };
+
+    const handleEditObject = (index: number) => {
+        const dataToUpdate = store.myData[index];
+        setLocalData({ name: dataToUpdate.name, address: dataToUpdate.address, phone: dataToUpdate.phone });
+        setIsEditing(true);
+        setEditingIndex(index);
+    };
+
+    const handleSaveObject = () => {
+        if (editingIndex >= 0) {
+            handleUpdateObject(editingIndex, localData);
+        }
+    };
+
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        const data = localStorage.getItem('myData');
+        if (data) {
+            store.setMyData(JSON.parse(data));
+        } else {
+            localStorage.setItem('myData', JSON.stringify(store.myData));
+        }
+    }, [store]);
+
+    useEffect(() => {
+        if (!isLoading) {
+            const data = localStorage.getItem('myData');
+            if (data) {
+                store.setMyData(JSON.parse(data));
+            }
+        }
+    }, [isLoading, store]);
+
+    return (
+
+        <div className="container">
+            
+            <div className='container1'>
+            <form className="form" onSubmit={handleAddObject}>
+            <h1 className="title"> Laboratorul 5.2</h1>
+                <input
+                    className="input"  type="text" placeholder="Nume" defaultValue={localData.name} onChange={(e) => setLocalData({ ...localData, name: e.target.value })}
+                />
+                <input
+                    className="input" type="text" placeholder="Adresa" defaultValue={localData.address} onChange={(e) => setLocalData({ ...localData, address: e.target.value })}
+                />
+                <input
+                    className="input" type="text" placeholder="Telefon" defaultValue={localData.phone} onChange={(e) => setLocalData({ ...localData, phone: e.target.value })}
+                />
+                <button className="button" type="submit">{isLoading ? 'Se incarca...' : 'Adauga'}</button>
+
+            </form>
+            <table className="table">
+                <thead>
+                <tr>
+                    <th>Nume</th>
+                    <th>Adresa</th>
+                    <th>Telefon</th>
+                    <th>Actiuni</th>
+                </tr>
+                </thead>
+                <tbody>
+                {myData.map((item: any, index: number) => (
+                    <tr key={index}>
+                        <td>{item.name}</td>
+                        <td>{item.address}</td>
+                        <td>{item.phone}</td>
+                        <td>
+                            <button className="button edit" onClick={() => handleEditObject(index)}>
+                                Editeaza
+                            </button>
+                            <button className="button delete" onClick={() => handleDeleteObject(index)}>
+                                Sterge
+                            </button>
+                        </td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+            {isEditing && (
+                <div className="edit-form">
+                    <h2>Editare</h2>
+                    <form onSubmit={handleSaveObject}>
+                        <input
+                            className="input"
+                            type="text"
+                            placeholder="Nume"
+                            value={localData.name}
+                            onChange={(e) => setLocalData({ ...localData, name: e.target.value })}
+                        />
+                        <input
+                            className="input"
+                            type="text"
+                            placeholder="Adresa"
+                            value={localData.address}
+                            onChange={(e) => setLocalData({ ...localData, address: e.target.value })}
+                        />
+                        <input
+                            className="input"
+                            type="text"
+                            placeholder="Telefon"
+                            value={localData.phone}
+                            onChange={(e) => setLocalData({ ...localData, phone: e.target.value })}
+                        />
+                        <div className="edit-form-buttons">
+                            <button className="button save" type="submit">
+                                Salveaza
+                            </button>
+                            <button className="button cancel" onClick={() => setIsEditing(false)}>
+                                Anuleaza
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            )}
+        </div>
+        </div>
+    );
 }
 
-export default App;
+export default MyComponent;
